31e09467954c1d5c5b9b7ef894685992df0696b5
irobato4-collab <irobato4@gmail.com>
2026-01-30 09:28:18 +0900

    Update client.js

--- public/client.js
+++ public/client.js
@@ -51,6 +51,20 @@ const KEY_AVATAR = "chat_avatar";
 const KEY_UID = "chat_user_id";
 const KEY_LAST_ACTIVE = "chat_last_active";
 
+let lastActive = Number(localStorage.getItem(KEY_LAST_ACTIVE) || 0);
+
+function updateLastActive() {
+  lastActive = Date.now();
+  localStorage.setItem(KEY_LAST_ACTIVE, String(lastActive));
+}
+
+window.addEventListener("focus", updateLastActive);
+document.addEventListener("visibilitychange", () => {
+  if (!document.hidden) updateLastActive();
+});
+
+// 初回
+updateLastActive();
 // 起動時
 let lastActive = Number(localStorage.getItem(KEY_LAST_ACTIVE) || 0);
 
